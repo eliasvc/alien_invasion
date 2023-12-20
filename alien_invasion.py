fead2a9de541af36fae1f0e96aa126aa36821df8
Elias <elias.vc@gmail.com>
2023-12-20 18:15:30 -0500

    add ship class and ship image. Also added the ship to the AlienInvasion class for drawing

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overal class to manage game assets and behavior"""
@@ -12,11 +13,13 @@ class AlienInvasion:
 
         # Add clock for framerate
         self.clock = pygame.time.Clock()
-        
+
         self.settings = Settings()
         # Create display window
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -28,6 +31,7 @@ class AlienInvasion:
             
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
