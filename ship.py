5f06edfd80a554d37f5a1e0f2613e4f3766eba32
Elias <elias.vc@gmail.com>
2023-12-22 21:46:04 -0500

    add range limit to ship so it doesn't go off-screen

--- ship.py
+++ ship.py
@@ -29,9 +29,9 @@ class Ship:
     
     def update(self):
         """Update ship's movement based on key press"""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         """Update rect object from self.x"""
