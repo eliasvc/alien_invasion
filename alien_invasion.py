bc1dcb664edf48ec76f7abb3160705ba6ed5eb9f
Elias <elias.vc@gmail.com>
2023-12-22 22:38:12 -0500

    refactor _check_events by adding a few helper fuctions

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,44 +48,63 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
             # Handle joystick hotplugging
             elif event.type == pygame.JOYDEVICEADDED:
-                # From the pygame offical docs:
-                # This event will be generated when the program starts for every
-                # joystick, filling up the list without needing to create them manually.
-                self.joycon = pygame.joystick.Joystick(event.device_index)
-                print(f"Joystick {self.joycon.get_instance_id()} connencted")
+                self._check_joystick_connection_events(event)
             elif event.type == pygame.JOYDEVICEREMOVED:
-                self.joycon = None
-                print(f"Joystick {event.instance_id} disconnected")
+                self._check_joystick_disconnection_events(event)
             elif event.type == pygame.JOYHATMOTION and self.joycon:
-                # hat contains a touple with values depending on which dpad button was pressed:
-                # (1, 0)  -> dpad right
-                # (-1, 0) -> dpad left
-                # (0, 1)  -> dpad up
-                # (0, -1) -> dpad down
-                # Also, hat comes from the physical hat switch the dpad uses on xbox and ps controllers
-                # xbox and ps controllers only have on dpad, hence get_hat(0)
-                hat_x, hat_y = self.joycon.get_hat(0)
-                if hat_x == 1:
-                    self.ship.moving_right = True
-                if hat_x == -1:
-                    self.ship.moving_left = True
-                if hat_x == 0:
-                    self.ship.moving_right = False
-                    self.ship.moving_left = False
+                self._check_hat_events(event)
             else:
                 print(event)
+    
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+    
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
+    def _check_joystick_connection_events(self, event):
+        """Register joystick device when connected."""
+        # From the pygame offical docs:
+        # This event will be generated when the program starts for every
+        # joystick, filling up the list without needing to create them manually.
+        self.joycon = pygame.joystick.Joystick(event.device_index)
+        print(f"Joystick {self.joycon.get_instance_id()} connencted")
+    
+    def _check_joystick_disconnection_events(self, event):
+        """Remove joystick device from game when disconnected."""
+        self.joycon = None
+        print(f"Joystick {event.instance_id} disconnected")
+    
+    def _check_hat_events(self, event):
+        """Respond to hat (d-pad) presses."""
+        # hat contains a touple with values depending on which dpad button was pressed:
+        # (1, 0)  -> dpad right
+        # (-1, 0) -> dpad left
+        # (0, 1)  -> dpad up
+        # (0, -1) -> dpad down
+        # Also, hat comes from the physical hat switch the dpad uses on xbox and ps controllers
+        # xbox and ps controllers only have on dpad, hence get_hat(0)
+        hat_x, hat_y = self.joycon.get_hat(0)
+        if hat_x == 1:
+            self.ship.moving_right = True
+        if hat_x == -1:
+            self.ship.moving_left = True
+        if hat_x == 0:
+            self.ship.moving_right = False
+            self.ship.moving_left = False        
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
